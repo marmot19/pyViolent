f46564c359ecfcc4d820d0f0bf8924130225e70a
marmot19 <zrhayy@gmail.com>
2016-02-27 13:29:25 +0800

    socekt pyviolent

--- pyTrain/csdc/zrh/prime/__init__.py
+++ pyTrain/csdc/zrh/prime/__init__.py
@@ -1,4 +1,5 @@
 import socket
+# 变量
 port=21
 banner = "FreeFloat FTP Server"
 portList = [21, 22, 80, 110]
@@ -12,7 +13,6 @@ print ("[+] Checking for " + banner + "on port " + str(port))
 # portList.append(5)
 # portList.sort()
 # print (portList)
-
 pos = portList.index(80)
 print ("There are " + str(pos) + " ports to scan before 80.")
 services = {'ftp':21, 'ssh':22, 'smtp':25, 'http':80}
@@ -21,6 +21,10 @@ services = {'ftp':21, 'ssh':22, 'smtp':25, 'http':80}
 # print ('ftp' in services)
 # print (services['ftp'])
 print ("[+] Found vuln with FTP on port " + str(services['ftp']))
+# python socket测试，本地防火墙、FTP服务都打开，还是连不上？
 socket.setdefaulttimeout(2)
 s = socket.socket()
+s.connect(("127.0.0.1", 21))
+ans = s.recv(1024)
+print (ans)
 
